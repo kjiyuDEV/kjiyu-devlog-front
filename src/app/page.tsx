09f9561504852c7e959ff93abfc9a93363b0105f
kjiyuDEV <kjiyu1379@gmail.com>
2024-11-20 00:31:52 +0900

    Deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,97 +9,98 @@ import { TYPE } from './_components/types';
 import SignUpModal from './_components/modals/SignUpModal';
 
 export default function Home() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const { modal, auth } = useSelector((state: RootState) => {
-    return {
-      modal: state.modals.modal,
-      auth: state.auth,
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const { modal, auth } = useSelector((state: RootState) => {
+        return {
+            modal: state.modals.modal,
+            auth: state.auth,
+        };
+    });
+
+    const handleLoginModal = () => {
+        if (auth.isAuthenticated) {
+            dispatch({
+                type: TYPE.OPEN_CONFIRM_MODAL,
+                data: {
+                    type: 'logout',
+                    content: '로그아웃 하시겠어요?',
+                },
+            });
+        }
+        if (!auth.isAuthenticated) {
+            dispatch({
+                type: TYPE.OPEN_MODAL,
+                data: {
+                    type: 'login',
+                    title: 'Login',
+                },
+            });
+        }
     };
-  });
 
-  const handleLoginModal = () => {
-    if (auth.isAuthenticated) {
-      dispatch({
-        type: TYPE.OPEN_CONFIRM_MODAL,
-        data: {
-          type: 'logout',
-          content: '로그아웃 하시겠어요?',
-        },
-      });
-    }
-    if (!auth.isAuthenticated) {
-      dispatch({
-        type: TYPE.OPEN_MODAL,
-        data: {
-          type: 'login',
-          title: 'Login',
-        },
-      });
-    }
-  };
+    const handleSignUpModal = () => {
+        if (!auth.isAuthenticated) {
+            dispatch({
+                type: TYPE.OPEN_MODAL,
+                data: {
+                    type: 'signUp',
+                    title: '회원가입',
+                },
+            });
+        }
+    };
 
-  const handleSignUpModal = () => {
-    if (!auth.isAuthenticated) {
-      dispatch({
-        type: TYPE.OPEN_MODAL,
-        data: {
-          type: 'signUp',
-          title: '회원가입',
-        },
-      });
-    }
-  };
+    return (
+        <div className="main-wrapper">
+            <div className="main-wrap">
+                <h1>Kjiyu-dev-log</h1>
+                <h3>frontend-developer</h3>
+                <Image
+                    src="https://cdn1.iconfinder.com/data/icons/3d-isometric-color/512/computer-iso-color.png"
+                    alt="main-illust"
+                    width="300"
+                    height="300"
+                    unoptimized={true}
+                />
+                <p>일상 및 개발일지를 기록 합니다.</p>
+                <p>React/Typescript/NextJs, Node</p>
+            </div>
 
-  return (
-    <div className="main-wrapper">
-      <div className="main-wrap">
-        <h1>Kjiyu-dev-log</h1>
-        <h3>frontend-developer</h3>
-        <Image
-          src="https://cdn1.iconfinder.com/data/icons/3d-isometric-color/512/computer-iso-color.png"
-          alt="main-illust"
-          width="300"
-          height="300"
-        />
-        <p>일상 및 개발일지를 기록 합니다.</p>
-        <p>React/Typescript/NextJs, Node</p>
-      </div>
+            <div className="sign-wrap">
+                <button
+                    onClick={() => {
+                        handleLoginModal();
+                    }}
+                >
+                    {auth.isAuthenticated ? 'Logout' : 'Login'}
+                </button>
+                <div className="new-container">
+                    {!auth.isAuthenticated && (
+                        <>
+                            <p>New here?</p>
+                            <button
+                                onClick={() => {
+                                    handleSignUpModal();
+                                }}
+                            >
+                                Register
+                            </button>
+                        </>
+                    )}
 
-      <div className="sign-wrap">
-        <button
-          onClick={() => {
-            handleLoginModal();
-          }}
-        >
-          {auth.isAuthenticated ? 'Logout' : 'Login'}
-        </button>
-        <div className="new-container">
-          {!auth.isAuthenticated && (
-            <>
-              <p>New here?</p>
-              <button
-                onClick={() => {
-                  handleSignUpModal();
-                }}
-              >
-                Register
-              </button>
-            </>
-          )}
-
-          <p
-            className="just-view"
-            onClick={() => {
-              router.push('/list');
-            }}
-          >
-            {!auth.isAuthenticated ? 'Just View' : 'Go List'}
-          </p>
+                    <p
+                        className="just-view"
+                        onClick={() => {
+                            router.push('/list');
+                        }}
+                    >
+                        {!auth.isAuthenticated ? 'Just View' : 'Go List'}
+                    </p>
+                </div>
+            </div>
+            {modal.data.type === 'login' && <LoginModal />}
+            {modal.data.type === 'signUp' && <SignUpModal />}
         </div>
-      </div>
-      {modal.data.type === 'login' && <LoginModal />}
-      {modal.data.type === 'signUp' && <SignUpModal />}
-    </div>
-  );
+    );
 }
